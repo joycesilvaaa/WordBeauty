b2a83322a0f02c132805e5fe89cdb25978247b80
Joyce Silva <142221695+joycesilvaaa@users.noreply.github.com>
2024-06-13 17:00:28 -0300

    mudei de axios para fetch

--- Frontend/src/api/clienteApi.tsx
+++ Frontend/src/api/clienteApi.tsx
@@ -1,13 +1,10 @@
 import { CadastrarCliente, EditarCliente } from "../Interface/interface";
-import api from "../services/api";
 
 export async function listaClientes() {
     try {
-        const results = await api.get(`/clientes`);
-        if (!results) {
-            console.error(`Erro ao encontrar resultados`);
-        }
-        return results;
+        const response = await fetch('http://localhost:32832/clientes');
+        const data = await response.json();
+        return data;
     } catch (error) {
         console.error(`Erro ao listar os clientes: ${error}`);
     }
@@ -15,11 +12,9 @@ export async function listaClientes() {
 
 export async function clienteEspecifico(id: number) {
     try {
-        const results = await api.get(`/cliente/${id}`);
-        if (!results) {
-            console.error(`Nenhum resultado encontrado`);
-        }
-        return results;
+        const results = await fetch(`http://localhost:32832/cliente/${id}`);
+        const data = await results.json();
+        return data;
     } catch (error) {
         console.error(`Erro ao buscar cliente específico: ${error}`);
     }
@@ -27,35 +22,58 @@ export async function clienteEspecifico(id: number) {
 
 export async function cadastrarCliente(dadosCadastro: CadastrarCliente) {
     try {
-        const results = await api.post(`/cliente/cadastrar`, dadosCadastro);
-        if (!results) {
-            console.error(`Nenhum resultado encontrado`);
+        const results = await fetch(`http://localhost:32832/cliente/cadastrar`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(dadosCadastro)
+        });
+        console.log(results)
+        if (results.ok){
+            console.error(`Cliente cadastrado!`);
+            return { success: true, message: `Cliente cadastrado com sucesso.`}
         }
-        return results;
+        return { success: false, message: `Não foi possivel excluir o cliente.`};
     } catch (error) {
         console.error(`Erro ao cadastrar cliente: ${error}`);
     }
 }
 
-export async function excluirCliente(id: number) {
+export async function excluirCliente(cliente: number) {
     try {
-        const results = await api.get(`/cliente/excluir/${id}`);
-        if (!results) {
-            console.error(`Nenhum resultado encontrado`);
+        const results = await fetch('http://localhost:32832/cliente/excluir', {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ id: cliente})
+        })
+        console.log(results)
+        if (results.ok) {
+            console.error(`Cliente exluido!`);
+            return { success: true, message: `Cliente excluido com sucesso.`}
         }
-        return results;
+        return { success: false, message: `Não foi possivel excluir o cliente.`};
     } catch (error) {
         console.error(`Erro ao excluir cliente: ${error}`);
     }
 }
 
-export async function editaCliente(id: number, dadosUpdate: EditarCliente) {
+export async function editaCliente( dadosUpdate: EditarCliente) {
     try {
-        const results = await api.put(`/cliente/atualizar/${id}`, dadosUpdate);
-        if (!results) {
-            console.error(`Nenhum resultado encontrado`);
+        const results = await fetch(`http://localhost:32832/cliente/atualizar`, {
+            method: 'PUT',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(dadosUpdate)
+        });
+        if (results.ok){
+            console.error(`Cliente editado!`);
+            return { success: true, message: `Cliente editado com sucesso.`}
         }
-        return results;
+        return { success: false, message: `Não foi possivel editar o cliente.`};
     } catch (error) {
         console.error(`Erro ao atualizar cliente: ${error}`);
     }
